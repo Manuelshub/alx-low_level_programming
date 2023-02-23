1162c6b8c479112e9520d366673591260f85bdcd
Manuelshub <kcemmy03@gmail.com>
2023-02-22 18:29:48 -0800

    Sign task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - the function that prints signs
+ *
+ * @n: the function to printed
+ *
+ * Return: 1 0 -1 when successful
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
