15659d703755000ca51ed9e78f14137678a2e6d9
Manuelshub <kcemmy03@gmail.com>
2023-02-22 20:27:40 -0800

    a + b Task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - this add two numbers
+ *
+ * @a: this is parameter 1
+ *
+ * @b: this is parameter 2
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
