728dd098a0fa39951bc367c2dc145f985b1275f7
Manuelshub <kcemmy03@gmail.com>
2023-09-21 04:54:59 -0700

    modified strcmp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,10 +8,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2)
+
+	while (*s1 != '\0' || *s2 != '\0')
 	{
-		s1++;
-		s2++;
+		if (*s1 == *s2)
+		{
+			s1 = s1 + 1;
+			s2 = s2 + 1;
+		}
+		else
+		{
+			return (*s1 - *s2);
+		}
 	}
-	return (*s1 - *s2);
+	return (0);
 }
