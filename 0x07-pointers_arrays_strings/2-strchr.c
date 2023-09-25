2b8993a12ebcd5f08432dfc1a3c4624ee85778cf
Manuelshub <kcemmy03@gmail.com>
2023-09-25 05:38:19 -0700

    modified 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,13 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
+		if (*s != c)
+		{
+			s++;
+		}
 		if (*s == c)
 		{
 			return (s);
-		}
-		else
-		{
-			s++;
 		}
 	}
 	return (NULL);
