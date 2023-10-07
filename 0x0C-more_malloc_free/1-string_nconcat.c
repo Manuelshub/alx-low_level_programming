8bb499665ebd66eaf45af7e45769d44c8f7e2d10
Manuelshub <kcemmy03@gmail.com>
2023-10-07 03:15:43 -0700

    Modified concatenating function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,38 +2,50 @@
 
 /**
  * string_nconcat - concatenates two strings.
- * @s1: the pointer to the first string.
- * @s2: pointer to the string.
- * @n: number of bytes of s2.
+ * @s1: pointer to the first string.
+ * @s2: ppointer to the second string.
+ * @n: number of bytes of s2 to be added.
  * Return: pointer to the concatenated string.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
+	char *ptarr;
 	unsigned int i, len, len2;
 
 	if (s1 == NULL)
 		len = 0;
 	else
-		len = strlen(s1);
+	{
+		for (len = 0; s1[len]; len++)
+			;
+	}
 	if (s2 == NULL)
 		len2 = 0;
 	else
-		len2 = strlen(s2);
-	if (len2 > n)
+	{
+		for (len2 = 0; s2[len2]; len2++)
+			;
+	}
+	if (n >= len2)
+	{
+		ptarr = malloc(sizeof(char) * (len + len2 + 1));
+		if (ptarr == NULL)
+			return (NULL);
+	}
+	else
+	{
 		len2 = n;
-	arr = malloc(sizeof(char) * (len + len2 + 1));
-	if (arr == NULL)
-		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		arr[i] = s1[i];
+		ptarr = malloc(sizeof(char) * (len + len2 + 1));
+		if (ptarr == NULL)
+			return (NULL);
 	}
-	for (i = 0; i < len2; i++)
-	{
-		arr[i + len] = s2[i];
-	}
-	arr[len + len2] = '\0';
+	for (i = 0; i < len; i++)
+		ptarr[i] = s1[i];
 
-	return (arr);
+	for (i = 0; i < len2; i++)
+		ptarr[i + len] = s2[i];
+
+	ptarr[len + len2] = '\0';
+
+	return (ptarr);
 }
