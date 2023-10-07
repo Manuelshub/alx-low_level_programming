65d70d66f4bd563937fe761e93bdd1b724fe20e4
Manuelshub <kcemmy03@gmail.com>
2023-10-07 13:20:35 -0700

    Modified realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,6 +13,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	new = malloc(new_size);
+	if (new == NULL)
+		return (NULL);
 	if (new_size == old_size)
 	{
 		return (ptr);
