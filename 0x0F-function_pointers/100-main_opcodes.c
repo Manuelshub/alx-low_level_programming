89ca24de168d648ea6eebd27aa5ab08182c5226b
Manuelshub <kcemmy03@gmail.com>
2023-10-13 14:45:03 -0700

    Function thatprints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point.
+ * @ac: argument count.
+ * @av: argument vector.
+ * Return: 0.
+ */
+int main(int ac, char **av)
+{
+	int num_bytes, i;
+	void *main_add;
+	unsigned char *byte_arr;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num_bytes = atoi(av[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	main_add = (void *)main;
+	byte_arr = (unsigned char *)&main_add;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		if (i != 0)
+			printf(" ");
+		printf("%02x", byte_arr[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
