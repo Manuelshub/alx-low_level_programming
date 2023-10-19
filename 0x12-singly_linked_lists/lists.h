8432e2e258f62e6f15aa5cdfc169b47f4d7f3698
Manuelshub <kcemmy03@gmail.com>
2023-10-19 08:41:12 -0700

    Added some header files

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,6 +16,9 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
