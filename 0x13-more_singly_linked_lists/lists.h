cd97e587da04ff56c920bdabc89f99ba2a9651ca
Manuelshub <kcemmy03@gmail.com>
2023-10-24 04:42:06 -0700

    Added some header files

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -14,10 +17,6 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
