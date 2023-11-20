e66827ddb6ba545c1c0c11eb947ee6b590692bef
Emmanuel <kcemmy03@gmail.com>
2023-11-20 22:13:22 +0100

    Function that prints n times table starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,39 +9,41 @@ void print_times_table(int n)
 {
 	int i, j, mul;
 
-	if (n > 15 || n < 0)
-		_putchar('\n');
-	for (i = 0; i <= n; i++)
+	if (n <= 15 && n >= 0)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			mul = i * j;
-			if (mul == 0 && i == 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(mul + '0');
-				if (j == 9)
-					break;
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				mul = i * j;
+				if (j == 0)
+					_putchar(mul + '0');
+				else if (mul < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(mul + '0');
+				}
+				else if (mul >= 10 && mul < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(mul / 10 + '0');
+					_putchar((mul % 10) + '0');
+				}
+				else if (mul >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(mul / 100 + '0');
+					_putchar((mul / 10) % 10 + '0');
+					_putchar((mul % 10) + '0');
+				}
 			}
-			else if (mul == 0 && i >= 1)
-				_putchar(mul + '0');
-			else if (mul <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((mul % 10) + '0');
-			}
-			else if (mul > 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((mul / 10) + '0');
-				_putchar((mul % 10) + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
