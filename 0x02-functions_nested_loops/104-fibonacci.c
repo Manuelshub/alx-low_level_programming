2fe4129061cd3b6ff156d47e2ef00bc013770aa8
Emmanuel <kcemmy03@gmail.com>
2023-11-21 10:11:42 +0100

    Program that prints the 98th fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Return: 0.
+ */
+int main(void)
+{
+	unsigned long num1, num2, i, term, a, b, c;
+
+	num1 = 1, num2 = 2;
+	printf("%lu, %lu", a, b);
+	for (i = 0; i < 96; i++)
+	{
+		term = num1 + num2;
+		if (term < num2)
+			break;
+		num1 = num2;
+		num2 = term;
+		printf(", %lu", term);
+	}
+	a = num1 % 100;
+	num1 /= 100;
+	b = num2 % 100;
+	num2 /= 100;
+	for (; i < 96; i++)
+	{
+		c = a + b;
+		if (c > 100)
+		{
+			c %= 100;
+			term = num1 + num2 + 1;
+		}
+		else
+			term = num1 + num2;
+		num1 = num2;
+		num2 = term;
+		a = b;
+		b = c;
+		printf(", %lu%02lu", term, c);
+	}
+
+	printf("\n");
+	return (0);
+}
