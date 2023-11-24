a6f0b9518e0082185a0c451eb0ea05873e46b36c
Emmanuel <kcemmy03@gmail.com>
2023-11-24 11:48:04 +0100

    A 64-bit assembly program that prints a text followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,16 @@
-global main
-extern printf
+global main		; the gcc entry point
+extern printf	; the c function to be called
 
 section .data
-    hello_message db "Hello, Holberton", 10
+    hello_message db "Hello, Holberton", 0
+	hello_format db "%s", 10, 0
 
 section .text
-main:
+main:						; standard gcc entry point
     sub rsp, 8            ; Align the stack
 
-    mov rdi, hello_message
+    mov rdi, hello_format
+	mov rsi, hello_message
     xor rax, rax          ; Clear RAX register (no vector registers used)
     call printf
 
