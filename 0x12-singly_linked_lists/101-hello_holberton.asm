193f1baa03964311bcdb68cf2470320ca32ed5b9
Emmanuel <kcemmy03@gmail.com>
2023-11-24 11:56:26 +0100

    Fixed a line of code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,10 @@
-extern printf	; the c function to be called
-
 section .data
     hello_message db "Hello, Holberton", 0
 	hello_format db "%s", 10, 0
 
 section .text
 	global main				; the gcc entry point
+	extern printf			; the c function to be called
 
 main:						; standard gcc entry point
     sub rsp, 8            ; Align the stack
@@ -17,4 +16,3 @@ main:						; standard gcc entry point
 
     add rsp, 8            ; Restore the stack
     ret
-
