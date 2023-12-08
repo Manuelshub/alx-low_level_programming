8520faabe4c7753cde97fe076d08662558e52907
Emmanuel <kcemmy03@gmail.com>
2023-12-08 18:21:03 +0100

    Fixed a code fail

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			temp = temp->next;
 			i++;
 		}
-		if (i == idx)
+		if (temp != NULL)
 		{
 			new_node->next = temp;
 			new_node->prev = temp->prev;
