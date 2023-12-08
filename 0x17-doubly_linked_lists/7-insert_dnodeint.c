1925d9256ee26f33c54bca560825e4058cd65e71
Emmanuel <kcemmy03@gmail.com>
2023-12-08 18:27:19 +0100

    Fixed a code fail

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,12 +12,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp, *new_node;
 	unsigned int i = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
 	if (idx == 0)
 	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
 		new_node->next = *h;
 		new_node->prev = NULL;
 		if (*h != NULL)
@@ -27,24 +27,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	else
 	{
 		temp = *h;
-		while (temp != NULL && i < idx)
+		while (i < idx)
 		{
+			if (temp == NULL)
+				return (NULL);
 			temp = temp->next;
 			i++;
 		}
-		if (temp != NULL)
-		{
-			new_node->next = temp;
-			new_node->prev = temp->prev;
-			if (temp->prev != NULL)
-				temp->prev->next = new_node;
-			temp->prev = new_node;
-		}
-		else
-		{
-			free(new_node);
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
 			return (NULL);
-		}
+		new_node->n = n;
+		new_node->next = temp;
+		new_node->prev = temp->prev;
+		if (temp->prev != NULL)
+			temp->prev->next = new_node;
+		temp->prev = new_node;
 	}
 	return (new_node);
 }
