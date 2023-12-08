7dae3147c09a7f7df03495b0aee450475a49f3b6
Emmanuel <kcemmy03@gmail.com>
2023-12-08 14:38:09 +0100

    Fixed requirement issue

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,10 +14,13 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
 	if (head == NULL || *head == NULL)
+	{
 		*head = new_node;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		return (new_node);
+	}
 	temp = *head;
 	while (temp->next != NULL)
 	{
