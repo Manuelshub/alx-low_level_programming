93714d7030e80cdedf16b81c03b6a267680b3ef6
Emmanuel <kcemmy03@gmail.com>
2023-12-08 19:46:22 +0100

    Fixing a requirement issue

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -17,9 +17,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		if (temp == NULL)
 			return (-1);
-		temp =temp->next;
+		temp = temp->next;
 		index--;
-	}	
+	}
 	if (temp == *head)
 	{
 		*head = (*head)->next;
