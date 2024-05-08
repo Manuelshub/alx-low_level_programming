d497f729635033e1fecb5ea27935cdef83fca587
Emmanuel <kcemmy03@gmail.com>
2024-05-08 16:17:38 +0100

    Fix requirement issue

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,21 +1,27 @@
 #include "search_algos.h"
 
 /**
- * 
+ * interpolation_search - searches an array of integers using interpolation
+ * search algorithm.
  *
+ * @array: pointer to the first element of the array
+ * @size: the length of the array.
+ * @value: the value to search for.
+ *
+ * Return: the index of value otherwise -1
  */
 int interpolation_search(int *array, size_t size, int value)
 {
 	int high, low;
 	size_t pos;
-	
+
 	if (!array || size == 0)
 		return (-1);
 	high = (int)size - 1, low = 0;
 
 	while (value >= array[low] && value <= array[high] && low <= high)
 	{
-		pos = low + (((double)(high - low) / (array[high] - 
+		pos = low + (((double)(high - low) / (array[high] -
 						array[low])) * (value - array[low]));
 		if (array[pos] == value)
 		{
