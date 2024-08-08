863286e0b3d410b094134688ac5bc7cac2e98ea9
Emmanuel <kcemmy03@gmail.com>
2024-08-08 13:38:20 +0100

    Added a new function to code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 
 /**
+ * check_arg - Checks if a string contains only numbers.
+ * 
+ * @s: the string.
+ * 
+ * Return: 0 if it contains and 1 if not.
+ */
+int check_arg(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
  * main - Entry point.
  * @argc: argument count.
  * @argv: argument vector.
@@ -11,6 +32,7 @@ int main(int argc, char **argv)
 {
 	int i = 1;
 	int sum = 0;
+	char *argument;
 
 	if (argc <= 1)
 	{
@@ -20,15 +42,15 @@ int main(int argc, char **argv)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] < '0' || *argv[i] > '9')
+			argument = argv[i];
+
+			if (!check_arg(argument))
 			{
+				
 				printf("Error\n");
 				return (1);
 			}
-			else
-			{
-				sum += atoi(argv[i]);
-			}
+			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 	}
